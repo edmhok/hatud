137ad8e38f5d9b0fa9c51195ebab1d2fb89ad956
edmhok <121737342+edmhok@users.noreply.github.com>
2023-12-21 14:59:19 +0800

    close popup and clean event listener

--- src/components/utils/Popup.svelte
+++ src/components/utils/Popup.svelte
@@ -1,10 +1,39 @@
 <script>
+	import { beforeUpdate, afterUpdate, onMount } from 'svelte';
 	let isOpen = false;
+	onMount(() => {
+		addEventListener('click', closePopup);
+	});
+	beforeUpdate(() => {
+		console.log('before update called!');
+	});
+	afterUpdate(() => {
+		console.log('after update called!');
+	});
+	function closePopup() {
+		if (isOpen) isOpen = false;
+	}
 </script>
 
-<div class="relative">
-	<button on:click={() => (isOpen = true)}>Open Me </button>
+<div class="flex-it">
+	<div class="flex-it">
+		<button
+			on:click|stopPropagation={() => {
+				isOpen = true;
+			}}
+		>
+			<slot />
+		</button>
+	</div>
 	{#if isOpen}
-		<div class="flex-it w-20 h-20 fixed bg-black bottom-10 popup">Hello World</div>
+		<div
+			class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000"
+		>
+			<div class="w-72 min-w-68 max-h-120 min-h-8 flex-it overflow-auto">
+				<div class="flex-it flex-grow flex-shrink py-3">
+					<div class="flex-it px-4 py-3 transition hover:bg-gray-700">Logout</div>
+				</div>
+			</div>
+		</div>
 	{/if}
 </div>
