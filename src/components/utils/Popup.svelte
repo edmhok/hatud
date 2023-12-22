b9229969aa9f78e61fa8781ff41fa92f3a4e0cc6
edmhok <121737342+edmhok@users.noreply.github.com>
2023-12-22 20:33:08 +0800

    portal

--- src/components/utils/Popup.svelte
+++ src/components/utils/Popup.svelte
@@ -1,6 +1,10 @@
 <script>
 	import { beforeUpdate, afterUpdate, onMount, onDestroy } from 'svelte';
+	import Portal from './Portal.svelte';
+
 	let isOpen = false;
+	let openerMenu;
+	let popupBottomPosition;
 
 	onMount(() => {
 		addEventListener('click', closePopup);
@@ -13,9 +17,6 @@
 		console.log('destroy from onDestroy called!');
 	});
 
-	beforeUpdate(() => {
-		console.log('before update called!');
-	});
 	afterUpdate(() => {
 		popupBottomPosition = openerMenu.clientHeight + 'px';
 	});
@@ -26,7 +27,7 @@
 </script>
 
 <div class="flex-it">
-	<div class="flex-it">
+	<div bind:this={openerMenu} class="flex-it">
 		<button
 			on:click|stopPropagation={() => {
 				isOpen = true;
@@ -36,14 +37,17 @@
 		</button>
 	</div>
 	{#if isOpen}
-		<div
-			class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000"
-		>
-			<div class="w-72 min-w-68 max-h-120 min-h-8 flex-it overflow-auto">
-				<div class="flex-it flex-grow flex-shrink py-3">
-					<div class="flex-it px-4 py-3 transition hover:bg-gray-700">Logout</div>
+		<Portal>
+			<div
+				style="bottom: {popupBottomPosition}"
+				class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000"
+			>
+				<div class="w-72 min-w-68 max-h-120 min-h-8 flex-it overflow-auto">
+					<div class="flex-it flex-grow flex-shrink py-3">
+						<div class="flex-it px-4 py-3 transition hover:bg-gray-700">Logout</div>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Portal>
 	{/if}
 </div>
