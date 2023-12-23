fa01f19c2287a691a5a66452236736f2e6b057e7
edmhok <121737342+edmhok@users.noreply.github.com>
2023-12-23 09:33:49 +0800

    UI Context and Set context

---
+++ src/components/context/UI/UIContext.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { onMount, setContext } from 'svelte';
+	import { key } from './key';
+	let color = 'blue';
+
+	onMount(() => {
+		console.log('Context was mounted!');
+	});
+
+	setContext(key, {
+		getMessage: () => {
+			console.log('Hi there!');
+			return 'Hello World!';
+		},
+		color
+	});
+</script>
+
+<slot />
