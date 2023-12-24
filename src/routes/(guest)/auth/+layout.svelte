d454e41b371bff3ab3cad426cd6bfd4c844b0255
edmhok <121737342+edmhok@users.noreply.github.com>
2023-12-24 12:59:11 +0800

    redirect if authenticated

---
+++ src/routes/(guest)/auth/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { getAuthContext } from '@components/context/auth';
+	const { isAuthenticated } = getAuthContext();
+	if ($isAuthenticated) {
+		goto('/');
+	}
+</script>
+
+{#if !isAuthenticated}
+	<slot />
+{/if}
