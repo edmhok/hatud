f54723e642ea858f31235cfab19dc33a6ce0ae7c
edmhok <121737342+edmhok@users.noreply.github.com>
2023-12-25 12:02:36 +0800

    input event handlers

--- src/stores/createFormStore.js
+++ src/stores/createFormStore.js
@@ -1,29 +1,17 @@
-import { afterUpdate, beforeUpdate, onDestroy, onMount } from "svelte";
 import { writable } from "svelte/store";
 
 
 export function createFormStore(initialData) {
   const form = writable(initialData);
 
-  onMount(() => {
-    console.log("createFormStore onMount called!");
-  })
+  function validate(node) {
+    node.onblur = (e) => {
+      console.log(`blur on ${e.target.name} called with value ${e.target.value}`);
+    }
 
-  onDestroy(() => {
-    console.log("createFormStore onDestroy called!");
-  })
-
-  beforeUpdate(() => {
-    console.log("createFormStore beforeUpdate called!");
-  })
-
-  afterUpdate(() => {
-    console.log("createFormStore afterUpdate called!");
-  })
-
-  function validate(node, value) {
-    console.log(node);
-    console.log(value);
+    node.oninput = (e) => {
+      console.log(`onInput on ${e.target.name} called with value ${e.target.value}`);
+    }
   }
 
   return {
